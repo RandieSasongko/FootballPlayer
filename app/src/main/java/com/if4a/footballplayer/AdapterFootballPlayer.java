51b8e7b013e69a72231cc4d26f8472ef355a115a
hkh <>
2023-03-15 09:26:50 +0700

    15/03/2023

--- app/src/main/java/com/if4a/footballplayer/AdapterFootballPlayer.java
+++ app/src/main/java/com/if4a/footballplayer/AdapterFootballPlayer.java
@@ -7,6 +7,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -77,7 +78,18 @@ public class AdapterFootballPlayer extends RecyclerView.Adapter<AdapterFootballP
                     pesan.setNegativeButton("Hapus", new DialogInterface.OnClickListener() {
                         @Override
                         public void onClick(DialogInterface dialog, int which) {
-
+                            MyDatabaseHelper myDB = new MyDatabaseHelper(ctx);
+                            long eksekusi = myDB.hapusPlayer(tvId.getText().toString());
+                            if(eksekusi == -1)
+                            {
+                                Toast.makeText(ctx , "Gagal Hapus Data", Toast.LENGTH_SHORT).show();
+                            }
+                            else
+                            {
+                                Toast.makeText(ctx, "Berhasil Menghapus Data", Toast.LENGTH_SHORT).show();
+                                dialog.dismiss();
+                                ((MainActivity) ctx).onResume();
+                            }
                         }
                     });
 
